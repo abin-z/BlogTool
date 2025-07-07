72476041d52eb3c0e21fff28752a77863fbad080
abin <abin.z@outlook.com>
2025-07-07 23:59:49 +0800

    feat: update

--- blogtool/src/main.cpp
+++ blogtool/src/main.cpp
@@ -1,7 +1,16 @@
-#include "fmt/core.h"
+#include <fmt/core.h>
 
-int main()
+#include <CLI/CLI.hpp>
+
+int main(int argc, char** argv)
 {
-  fmt::print("Hello, world! This is my blog tool!\n");
+  CLI::App app{"A CLI + fmt demo"};
+
+  std::string name = "World";
+  app.add_option("-n,--name", name, "Name to greet");
+
+  CLI11_PARSE(app, argc, argv);
+
+  fmt::print("Hello, {}!\n", name);
   return 0;
 }
