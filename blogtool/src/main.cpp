9d6df39e47c794e58650979238bcb34558e15dff
abin <abin.z@outlook.com>
2025-07-08 00:17:49 +0800

    feat: add clear cmd

--- blogtool/src/main.cpp
+++ blogtool/src/main.cpp
@@ -1,9 +1,17 @@
 #include <fmt/core.h>
 
 #include <CLI/CLI.hpp>
+#include <cstdlib>
 
 int main(int argc, char** argv)
 {
+#ifdef _WIN32
+  std::system("chcp 65001 > nul");  // 设置控制台编码为 UTF-8，静默输出
+  std::system("cls");    // Windows 清屏
+#else
+  std::system("clear");  // Linux/macOS 清屏
+#endif
+
   CLI::App app{"A CLI + fmt demo"};
 
   std::string name = "World";
