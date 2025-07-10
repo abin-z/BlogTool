2bad30c034f1b1734377eee6b1d4893cecce647a
abin <abin.z@outlook.com>
2025-07-11 00:23:35 +0800

    fix: 修复中文崩溃问题

--- blogtool/src/main.cpp
+++ blogtool/src/main.cpp
@@ -1,27 +1,78 @@
+#ifdef _WIN32
+#include <windows.h>
+
+#include <string>
+#include <vector>
+
+#endif
+
 #include <fmt/core.h>
 
 #include <CLI/CLI.hpp>
 #include <cstdlib>
 
-// TODO: 解决中文输入崩溃问题
-// TODO: git submodule 无效问题
-
-int main(int argc, char** argv)
+// UTF-16 wchar_t* 转 UTF-8 std::string
+std::string utf16_to_utf8(const std::wstring& wstr)
 {
+  int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), NULL, 0, NULL, NULL);
+  std::string strTo(size_needed, 0);
+  WideCharToMultiByte(CP_UTF8, 0, wstr.data(), (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+  return strTo;
+}
+
+// 将 wmain 参数转换成 UTF-8 char* 数组
+std::vector<char*> convert_wargv_to_argv(int argc, wchar_t** wargv, std::vector<std::string>& storage)
+{
+  storage.clear();
+  storage.reserve(argc);
+  std::vector<char*> argv;
+
+  for (int i = 0; i < argc; ++i)
+  {
+    std::string arg = utf16_to_utf8(wargv[i]);
+    storage.push_back(std::move(arg));
+  }
+  for (int i = 0; i < argc; ++i)
+  {
+    argv.push_back(const_cast<char*>(storage[i].c_str()));
+  }
+  return argv;
+}
+
 #ifdef _WIN32
-  std::system("chcp 65001 > nul");  // 设置控制台编码为 UTF-8，静默输出
-  std::system("cls");    // Windows 清屏
-#else
-  std::system("clear");  // Linux/macOS 清屏
-#endif
+int wmain(int argc, wchar_t** wargv)
+{
+  SetConsoleOutputCP(CP_UTF8);
+  std::system("cls");
+
+  std::vector<std::string> utf8_args;
+  std::vector<char*> argv = convert_wargv_to_argv(argc, wargv, utf8_args);
 
   CLI::App app{"A CLI + fmt demo"};
 
   std::string name = "World";
-  app.add_option("-n,--name", name, "Name to greet"); // TODO -n 输入中文会崩溃
+  app.add_option("-n,--name", name, "Name to greet");
+
+  CLI11_PARSE(app, argc, argv.data());
+
+  fmt::print("Hello, {}!\n", name);
+
+  return 0;
+}
+#else
+int main(int argc, char** argv)
+{
+  std::system("clear");
+
+  CLI::App app{"A CLI + fmt demo"};
+
+  std::string name = "World";
+  app.add_option("-n,--name", name, "Name to greet");
 
   CLI11_PARSE(app, argc, argv);
 
   fmt::print("Hello, {}!\n", name);
+
   return 0;
 }
+#endif
